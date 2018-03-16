559cb725a4164c054d1d5ac8a488b578135fd7be
qinliGIT <1620504364@qq.co>
2018-03-16 17:43:57 +0800

    更新网络请求类以及进入APP判断 Date:2017:03:16 Author:MrQin

---
+++ app/src/main/java/com/example/mrqin/myapplication/utils/downUtils.java
@@ -0,0 +1,9 @@
+package com.example.mrqin.myapplication.utils;
+
+/**
+ * Created by Mrqin on 2018/3/16.
+ */
+
+public class downUtils {
+
+}
--- app/src/main/java/com/example/mrqin/myapplication/view/MyWebView.java
+++ app/src/main/java/com/example/mrqin/myapplication/view/MyWebView.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.example.mrqin.myapplication.R;
 
@@ -42,9 +45,14 @@ public class MyWebView extends BaseActivity {
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);// 开启 DOM storage API 功能
-        settings.setCacheMode(WebSettings.LOAD_DEFAULT);// 设置 缓存模式
-        settings.setAppCacheEnabled(true);// 启动缓存
 
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setUseWideViewPort(true);//设置webview推荐使用的窗口
+        settings.setLoadWithOverviewMode(true);//设置webview加载的页面的模式
+        settings.setDisplayZoomControls(false);//隐藏webview缩放按钮
+        settings.setAllowFileAccess(true); // 允许访问文件
+        settings.setBuiltInZoomControls(true); // 设置显示缩放按钮
+        settings.setSupportZoom(true); // 支持缩放
 
         Intent intent = getIntent();
         if (intent != null) {
@@ -58,5 +66,29 @@ public class MyWebView extends BaseActivity {
             webView.loadUrl(url);
         }
 
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                if (url.startsWith("https://down")) {
+
+                } else {
+                    view.loadUrl(url);
+                }
+                return true;
+            }
+        });
     }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webView.canGoBack()) {
+                webView.goBack();//返回上一页面
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
