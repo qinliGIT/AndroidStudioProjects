4040f57af91d17c046f0436d319b1fac52004215
zzj <123456>
2018-03-19 17:16:47 +0800

    1.集成极光推送，添加推送通知界面 2.集成图灵api接口，实现与机器人简单对话

--- app/src/main/java/com/example/mrqin/myapplication/utils/APPID.java
+++ app/src/main/java/com/example/mrqin/myapplication/utils/APPID.java
@@ -5,8 +5,13 @@ package com.example.mrqin.myapplication.utils;
  */
 
 public class APPID {
+    public static String url = "https://appid-apkk.xx-app.com/frontApi/getAboutUs?appid=911580001";
+
+    public static final String TURING_ID = "59310";
+    public static final String TURING_SCREAT = "9048743cef4b47f693d913753c2a206e";
     public static String APP_ID = "58707";
     public static String APP_SCREAT = "361a7423700f4e129fd78a08de40d6cb";
     /*获取网络数据*/
     public static final int MSG_DATA_SUCCESS_LOTTERY_FRG = 0X001;
+
 }
