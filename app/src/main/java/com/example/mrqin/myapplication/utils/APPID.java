5f450e84e48d52c454181663d7b200b66cf7d13d
qinliGIT <1620504364@qq.co>
2018-03-19 17:33:43 +0800

    更新bug Date:2017:03:19 Author:MrQin

--- app/src/main/java/com/example/mrqin/myapplication/utils/APPID.java
+++ app/src/main/java/com/example/mrqin/myapplication/utils/APPID.java
@@ -1,11 +1,11 @@
 package com.example.mrqin.myapplication.utils;
 
 /**
- * Created by Amos on 2018/3/14.
+ * Created by Mrqin on 2018/3/14.
  */
 
 public class APPID {
-    public static String url = "https://appid-apkk.xx-app.com/frontApi/getAboutUs?appid=911580001";
+    public static String url = "https://appid-apkk.xx-app.com/frontApi/getAboutUs?appid=91158001";
 
     public static final String TURING_ID = "59310";
     public static final String TURING_SCREAT = "9048743cef4b47f693d913753c2a206e";
